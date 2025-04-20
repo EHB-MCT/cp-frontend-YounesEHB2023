bc81027633cdffbddca5db562ec490afb799dc44
YounesEHB2023 <145571386+YounesEHB2023@users.noreply.github.com>
2025-04-20 21:36:58 +0200

    added a example for the projects page and css

--- courseproject/code-courseproject/src/App.jsx
+++ courseproject/code-courseproject/src/App.jsx
@@ -10,22 +10,20 @@ import Projects from "./pages/Projects.jsx";
 import Parallax from "./pages/Parallax.jsx";
 
 function App() {
-    return (
-        <>
-            <h1>React Router Demo</h1>
-
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Navbar />}>
-                        <Route index element={<Home />} />
-                        <Route path="projects" element={<Projects />} />
-                        <Route path="makingof" element={<MakingOf />} />
-                    </Route>
-                    <Route path="parallax" element={<Parallax />} />
-                </Routes>
-            </BrowserRouter>
-        </>
-    );
+	return (
+		<>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Navbar />}>
+						<Route index element={<Home />} />
+						<Route path="projects" element={<Projects />} />
+						<Route path="makingof" element={<MakingOf />} />
+					</Route>
+					<Route path="parallax" element={<Parallax />} />
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
 }
 
 export default App;
--- courseproject/code-courseproject/src/pages/Projects.jsx
+++ courseproject/code-courseproject/src/pages/Projects.jsx
@@ -3,6 +3,26 @@ function Projects() {
 		<div>
 			<h1>Projects</h1>
 			<p>This is the Projects page.</p>
+			<div style={{ display: "flex", flexWrap: "wrap", gap: "16px" }}>
+				<div
+					style={{ width: "400px", height: "250px", backgroundColor: "#ccc" }}
+				></div>
+				<div
+					style={{ width: "400px", height: "250px", backgroundColor: "#ccc" }}
+				></div>
+				<div
+					style={{ width: "400px", height: "250px", backgroundColor: "#ccc" }}
+				></div>
+				<div
+					style={{ width: "400px", height: "250px", backgroundColor: "#ccc" }}
+				></div>
+				<div
+					style={{ width: "400px", height: "250px", backgroundColor: "#ccc" }}
+				></div>
+				<div
+					style={{ width: "400px", height: "250px", backgroundColor: "#ccc" }}
+				></div>
+			</div>
 		</div>
 	);
 }
