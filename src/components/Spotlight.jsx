448e73c542a05daa49c7fa3de2b02035abf23449
YounesEHB2023 <145571386+YounesEHB2023@users.noreply.github.com>
2025-04-23 19:12:20 +0200

    changed the structure of spotlight to not have repetitive code and changed a little css

--- src/components/Spotlight.jsx
+++ src/components/Spotlight.jsx
@@ -1,46 +1,24 @@
 import React from "react";
 
 const Spotlight = ({ imageSrc, title, description, icon }) => {
-	return (
-		<div className="spotlight-wrapper">
-			<div className="spotlight-container">
-				<div className="spotlight-image">
-					<img src={"./src/images/ErWasEens_Logo.png"} alt={title} />
-				</div>
-				<div className="spotlight-content1">
-					<h2>{"Aladdin"}</h2>
-					<p>{"Younes Ben Ali"}</p>
-					<div className="spotlight-icon">
-						<img src={"./src/images/info_icon.png"} alt="Info Icon" />
-					</div>
-				</div>
-			</div>
-			<div className="spotlight-container2">
-				<div className="spotlight-image">
-					<img src={"./src/images/ErWasEens_Logo.png"} alt={title} />
-				</div>
-				<div className="spotlight-content">
-					<h2>{"Aladdin"}</h2>
-					<p>{"Younes Ben Ali"}</p>
-					<div className="spotlight-icon">
-						<img src={"./src/images/info_icon.png"} alt="Info Icon" />
-					</div>
-				</div>
-			</div>
-			<div className="spotlight-container3">
-				<div className="spotlight-image">
-					<img src={"./src/images/ErWasEens_Logo.png"} alt={title} />
-				</div>
-				<div className="spotlight-content">
-					<h2>{"Aladdin"}</h2>
-					<p>{"Younes Ben Ali"}</p>
-					<div className="spotlight-icon">
-                    <img src={"./src/images/info_icon.png"} alt="Info Icon" />
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+    return (
+        <div className="spotlight-wrapper">
+            {[1, 2, 3].map((_, index) => (
+                <div key={index} className="spotlight-container">
+                    <div className="spotlight-image">
+                        <img src={"./src/images/ErWasEens_Logo.png"} alt={title} />
+                    </div>
+                    <div className="spotlight-content">
+                        <h2>{"Aladdin"}</h2>
+                        <p>{"Younes Ben Ali"}</p>
+                        <div className="spotlight-icon">
+                            <img src={"./src/images/info_icon.png"} alt="Info Icon" />
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
 };
 
 export default Spotlight;
